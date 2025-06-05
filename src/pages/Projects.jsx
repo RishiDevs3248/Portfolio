33bb0b97882c62ec4f9fd1cb5b3650e100362e76
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-05 21:47:11 +0530

    prj component idea

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,17 +6,10 @@ function Projects() {
     return (
         <div className={`home-page h-screen w-full ${isDark ? "bg-black text-white" : "bg-gray-200 text-black "} flex justify-center`}>
             <div className={` ${isDark ? "bg-black text-white" : "bg-gray-200 text-black "} w-[1050px] my-[100px]`}>
-                <div className={` ${isDark ? "bg-black text-white" : "bg-gray-200 text-black "} border rounded-[15px] p-[50px]`}>
-                    prj
-                </div>
-                <div>
-                    Skills
-                </div>
-                <div>
-                    Experience
-                </div>
-            </div>
+                {/* Page code below */}
+                {/* make a project component and reuse it  */}
 
+            </div>
         </div>
     );
 }
