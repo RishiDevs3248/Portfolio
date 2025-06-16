28f22208f59900b9e4dfb7fff8f2d70a88b0afa7
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-16 19:38:59 +0530

    Projrct block update

--- src/pages/ProjectBlock.jsx
+++ src/pages/ProjectBlock.jsx
@@ -1,13 +1,17 @@
-export default function ProjectBlock({prj_name , prj_img_src }){
+export default function ProjectBlock({ prj_name, prj_img_src, side = "l" }) {
     return (
         <>
-        {/* use ternary operator here in a way that aligns with right and left idea */}
-            <div className="border border-white rounded-md w-[75%] ">
+            {/* use ternary operator here in a way that aligns with right and left idea */}
+            <div className={`border border-white rounded-md w-[75%] flex  ${side == "r"?"flex-row-reverse":"row"}`}>
+                {/* prj image */}
                 <div>
                     <img src={prj_img_src}></img>
                 </div>
-                <div>{prj_img_src}</div>
-                <div>{prj_name}</div>
+                <div className={`flex justify-center items-center`}>
+                    {/* prj image name */}
+                    {/* <div>{prj_img_src}</div> */}
+                    <div>{prj_name}</div>
+                </div>
 
             </div>
         </>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,20 +7,24 @@ function Projects() {
 
     return (
         <div className={`home-page h-screen w-full ${isDark ? "bg-black text-white" : "bg-gray-200 text-black "} flex justify-center`}>
-            {/* this is not an efficient way (below code ) */}
+            <div className={` ${isDark ? "bg-black text-white" : "bg-gray-200 text-black "} w-[1050px] my-[100px] `}>
+                {/* this is not an efficient way (below code ) */}
 
-            {/* <div className={` ${isDark ? "bg-black text-white" : "bg-gray-200 text-black "} w-[1050px] my-[100px] `}>
-                Page code below
+                {/* Page code below
                 <div className={`left-prj`}>
                     <ProjectBlock prj_img_src="/TechLogo/TechLogo-1.jpg" prj_name="Hello" />
                 </div>
                 <div className={`right-prj`}>
                     <ProjectBlock prj_img_src="/TechLogo/TechLogo-1.jpg" prj_name="Hello" />
-                </div>
-            </div> */}
+                </div> */}
 
 
-            {/* try using something like ternary operator  and use it inside projectBlock so you dont have to change your code in many ways*/}
+                {/* try using something like ternary operator  and use it inside projectBlock so you dont have to change your code in many ways*/}
+
+                <ProjectBlock prj_img_src="/TechLogo/TechLogo-1.jpg" prj_name="Hello"  ></ProjectBlock>
+
+
+            </div>
         </div>
     );
 }
